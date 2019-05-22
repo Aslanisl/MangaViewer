68368c2b695dae079f6c7abfa2d80607824895f2
Ivan Kataev <aslanisl@mail.ru>
2019-05-22 18:03:41 +0300

    BottomNavView save state

--- app/src/main/java/ru/mail/aslanisl/mangareader/features/main/BottomNavView.kt
+++ app/src/main/java/ru/mail/aslanisl/mangareader/features/main/BottomNavView.kt
@@ -2,12 +2,17 @@ package ru.mail.aslanisl.mangareader.features.main
 
 import android.content.Context
 import android.graphics.PorterDuff
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.widget.FrameLayout
 import android.widget.ImageView
 import kotlinx.android.synthetic.main.custom_nav_view.view.*
 import ru.mail.aslanisl.mangareader.R
 import ru.mail.aslanisl.mangareader.getColorCompat
+import android.os.Bundle
+
+private const val KEY_SUPER_BUNDLE = "KEY_SUPER_BUNDLE"
+private const val KEY_CURRENT_ITEM = "KEY_CURRENT_ITEM"
 
 class BottomNavView
 @JvmOverloads constructor(
@@ -33,12 +38,39 @@ class BottomNavView
     fun selectItem(item: MainItem) {
         if (currentItem == item) return
         listener?.invoke(item)
-        updateColorItem(search, item == MainItem.SEARCH)
-        updateColorItem(genre, item == MainItem.GENRE)
-        updateColorItem(history, item == MainItem.HISTORY)
+        currentItem = item
+        updateItems()
+    }
+
+    private fun updateItems() {
+        updateColorItem(search, currentItem == MainItem.SEARCH)
+        updateColorItem(genre, currentItem == MainItem.GENRE)
+        updateColorItem(history, currentItem == MainItem.HISTORY)
     }
 
     private fun updateColorItem(view: ImageView, select: Boolean) {
         view.setColorFilter(if (select) selectColor else unselectColor, PorterDuff.Mode.OVERLAY)
+    }
+
+    override fun onSaveInstanceState(): Parcelable {
+        val bundle = Bundle()
+        bundle.putParcelable(KEY_SUPER_BUNDLE, super.onSaveInstanceState())
+        bundle.putString(KEY_CURRENT_ITEM, currentItem.name)
+        return bundle
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        var superState = state
+        if (state is Bundle) {
+            currentItem = try {
+                MainItem.valueOf(state.getString(KEY_CURRENT_ITEM) ?: "")
+            } catch (e: Exception) {
+                MainItem.SEARCH
+            }
+            superState = state.getParcelable(KEY_SUPER_BUNDLE)
+        }
+        updateItems()
+
+        super.onRestoreInstanceState(superState)
     }
 }
